8d55861f0849b8c5204deeba37e135c2623bcf63
msaisucharith <msaisucharith@gmail.com>
2025-03-16 07:00:27 +0530

    Linked list operation

--- DSA_LEVEL1/EASY/Linkedlist.js
+++ DSA_LEVEL1/EASY/Linkedlist.js
@@ -11,10 +11,62 @@ class Linkedlist {
     this.size = 0;
   }
 
+  isEmpty() {
+    return this.size == 0;
+  }
+
+  MiddeleNode() {
+    let mid = Math.floor(this.size / 2);
+    let prev = this.head;
+
+    for (let i = 0; i < mid - 1; i++) {
+      prev = prev.next;
+    }
+
+    return prev.value;
+  }
+
+  ReverseLinked() {
+    let prev = null;
+    let cur = this.head;
+    let next;
+
+    while (cur !== null) {
+      next = cur.next;
+
+      cur.next = prev;
+
+      prev = cur;
+      cur = next;
+    }
+
+    this.head = prev;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
   insert(value, index) {
     let node = new Node(value);
     if (this.size == 0) {
-      this.head = node;
+      this.prepend(value);
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
     }
   }
 
@@ -26,7 +78,7 @@ class Linkedlist {
       let current = this.head;
 
       while (current) {
-        value += `${current.value}`;
+        value += `${current.value}${" "}`;
         current = current.next;
       }
       return value;
@@ -35,6 +87,11 @@ class Linkedlist {
 }
 
 let list = new Linkedlist();
-list.insert(0);
-list.insert(1);
+list.insert(1, 0);
+list.insert(2, 1);
+list.insert(3, 2);
+
 console.log(list.printlist());
+console.log(list.ReverseLinked());
+console.log(list.printlist());
+console.log(list.MiddeleNode());
