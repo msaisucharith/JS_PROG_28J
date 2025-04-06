541a8876bb3c1ae808e6e155012943a0a5f4281b
msaisucharith <msaisucharith@gmail.com>
2025-04-06 15:02:53 +0530

    Binary

---
+++ DSA_LEVEL1/EASY/Async.js
@@ -0,0 +1,7 @@
+async function grtdata() {
+  const res = await fetch("https://catfact.ninja/fact");
+  const data = await res.json();
+  console.log(data.fact);
+}
+
+grtdata();
---
+++ DSA_LEVEL1/EASY/Binary.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] < target) {
+      // Search the right half
+      start = middle + 1;
+    } else if (arr[middle] > target) {
+      // Search the left half
+      end = middle - 1;
+    } else if (arr[middle] === target) {
+      // Found target
+      return middle;
+    }
+  }
+  // Target not found
+  return -1;
+}
+console.log(binarySearch([1, 2, 3, 4], 1));
